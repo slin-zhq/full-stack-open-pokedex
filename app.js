10803e1a28e25e59269d22eb97ba8817ef3560c7
Saw S. Lin <sawsanlin.zhiqi@gmail.com>
2023-10-17 18:46:11 +0630

    Exercise 11.19.: Attempt to fail the health check

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const PORT = process.env.PORT || 5001
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  throw 'error...'
+  // throw 'error...'
   // eslint-disable-next-line no-unreachable
-  // res.send('ok')
+  res.status(500)
 })
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
+  res.send('1; health check failed') // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
